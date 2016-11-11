65be5988bd8511bfadefbc7e20c9c6c668e9d914
Uladzislau Kavaliou <Uladzislau_Kavaliou@epam.com>
2016-11-11 13:30:24 +0300

    Adds check route for the error.

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 const { PORT, PREFIX } = require('./server/config');
 const { api, users } = require('./server/routes');
 
 app.use(morgan('dev'))
+  .use(cors())
   .use(`/${PREFIX}/`, api)
   .use(`/${PREFIX}/users`, users)
   .use((err, req, res, next) => {
-    console.log(err);
     res.status(200).json({
-      message: 'Error server'
+      message: err.message,
+      name: err.name
     });
   });
 
--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'OK'
   });
+});
+
+router.get('/error', (req, res, next) => {
+  next(new Error('Server error'));
 });
 
 module.exports = router;
